b3595c96add41a4a3f298a00b130474505bc7f08
Valentina Alarcón <63269085+valentinalarcon@users.noreply.github.com>
2020-04-25 20:48:48 -0400

    v 1.6

--- core/static/core/js/grafos.js
+++ core/static/core/js/grafos.js
@@ -1,22 +1,38 @@
 const padre = document.querySelector("#nArista");
-const numVertices = document.querySelector("#vertices");
 const arista = document.querySelector("#arista");
-const inputArista = document.querySelector("#inputArista").addEventListener("click",totalArista);
-const tipoGrafo = document.querySelector("#tipoGrafo");
-
-function totalArista(e){
-    e.preventDefault();
+const vertices = document.querySelector("#vertices");
+const inputArista = document.querySelector("#inputArista").addEventListener("click",IngresarAristas);
     
-    for(let i=1 ; i<=arista.value ; i++){
+var select = document.getElementById("tipoGrafo");
+select.addEventListener('change',tipoGrafo);
+    
+function tipoGrafo(){
+    var selectedOption = this.options[select.selectedIndex];
+    console.log(selectedOption.value);
+    };
+    
+    
+function IngresarAristas(){
+    document.querySelector("#inputArista").removeEventListener("click",IngresarAristas);
+    const textArista = document.createElement('div');
+    textArista.className="col-8";
+    textArista.innerHTML="Aristas";
+    padre.appendChild(textArista);
+    console.log(vertices.value); //print vertices!!
+    for(let i=1 ; i<vertices.value ; i++){
         const div = document.createElement('div');
         const div1 = document.createElement('div');
         const div2 = document.createElement('div');
         const label = document.createElement('label');
         const input = document.createElement('input');
-        div.className="d-flex";
-        div1.className="col-8 d-flex justify-content-end";
+        const quit = document.createElement('button');
+        div.className="row my-1";
+        div1.className="col-6 d-flex justify-content-end";
         label.className="pt-2";
-        label.textContent=`Arista e${i}`;
+        quit.className = 'btn btn-danger mx-1.8';
+        quit.innerText = '-';
+        quit.id=`btnQuit_e${i}`;
+        //label.textContent=`Arista ${i}`;
         div2.className="col-4";
         input.className="form-control mx-2";
         input.id=`vertice_e${i}`;
@@ -27,11 +43,17 @@ function totalArista(e){
         div2.appendChild(input);
         div.appendChild(div1);
         div.appendChild(div2);
+        div.appendChild(quit);
         padre.appendChild(div);
     }
 }
+/*
+var id_botones = document.getElementsByClassName("btn btn-danger mx-1.8");
+console.log(id_botones);
+id_botones.onclick = function(){
+ }
 
-    
+*/
 
 const grafo = document.querySelector("#inputGrafo");
 grafo.addEventListener('click',validar);
@@ -49,6 +71,7 @@ function validar(e){
         aristas : arista.value,
         allAristas : listaAristas
     }
+
     var json = JSON.stringify(Constuir);
     
 }
