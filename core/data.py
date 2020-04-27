c819f80d482597cd265d3f8aa634577f4d3c5a40
LeoCuantico <leo.kpex@gmail.com>
2020-04-26 22:29:13 -0400

    v1.7

--- core/data.py
+++
@@ -1,4 +0,0 @@
-import json
-
-json.loads(json);
-print(json)
--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from core import views as core_views
-
-
-urlpatterns = [
-    path('', core_views.home, name="home"),
-    path('grafos/', core_views.grafos, name="grafos"),
-    path('nosotros/', core_views.nosotros, name="nosotros"),
-]
