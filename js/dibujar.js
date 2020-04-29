7426686b4e22551288dd7190b5c232478771274a
LeoCuantico <leo.kpex@gmail.com>
2020-04-28 22:37:09 -0400

    v2.1

--- js/dibujar.js
+++ js/dibujar.js
@@ -1,60 +1,65 @@
 var nodes = null;
-    var edges = null;
-    var network = null;
-    // randomly create some nodes and edges
-    var data = getScaleFreeNetwork(25);
-    var seed = 2;
-    var defaultLocal = navigator.language;
-    var select = document.getElementById('locale');
+var edges = null;
+var network = null;
 
-    function destroy() {
-      if (network !== null) {
-        network.destroy();
-        network = null;
-      }
-    }
+var vertices = [];
+var aristas_from  = [];
+var aristas_to = [];
+// randomly create some nodes and edges
+var data = getScaleFreeNetwork(25);
+var seed = 2;
+var defaultLocal = navigator.language;
+var select = document.getElementById('locale');
 
-    function draw() {
-      destroy();
-      nodes = [];
-      edges = [];
+function destroy() {
+  if (network !== null) {
+    network.destroy();
+    network = null;
+  }
+}
 
-      // create a network
-      var container = document.getElementById('resultado');
-      var options = {
-        layout: {randomSeed:seed}, // just to make sure the layout is the same when the locale is changed
-        locale: document.getElementById('locale').value,
-        manipulation: {
-          addNode: function (data, callback) {
-            // filling in the popup DOM elements
-            document.getElementById('node-operation').innerHTML = "Agregar Vértice";
-            editNode(data, clearNodePopUp, callback);
-          },
-          editNode: function (data, callback) {
-            // filling in the popup DOM elements
-            document.getElementById('node-operation').innerHTML = "Editar Vértice";
-            editNode(data, cancelNodeEdit, callback);
-          },
-          addEdge: function (data, callback) {
-            if (data.from == data.to) {
-              var r = confirm("Do you want to connect the node to itself?");
-              if (r != true) {
-                callback(null);
-                return;
-              }
-            }
-            document.getElementById('edge-operation').innerHTML = "Agregar Arista";
-            editEdgeWithoutDrag(data, callback);
-          },
-          editEdge: {
-            editWithoutDrag: function(data, callback) {
+function draw() {
+  destroy();
+  nodes = [];
+  edges = [];
+  // create a network
+  var container = document.getElementById('resultado');
+  var options = {
+    layout: {randomSeed:seed}, // just to make sure the layout is the same when the locale is changed
+    locale: document.getElementById('locale').value,
+    manipulation: {
+      addNode: function (data, callback) {
+      // filling in the popup DOM elements
+        document.getElementById('node-operation').innerHTML = "Agregar Vértice";
+        editNode(data, clearNodePopUp, callback);
+      },
+      editNode: function (data, callback) {
+        // filling in the popup DOM elements
+        document.getElementById('node-operation').innerHTML = "Editar Vértice";
+        editNode(data, cancelNodeEdit, callback);
+      },
+      addEdge: function (data, callback) {
+        if (data.from == data.to) {
+          var r = confirm("Do you want to connect the node to itself?");
+          if (r != true) {
+            callback(null);
+            return;
+          }
+        }
+        document.getElementById('edge-operation').innerHTML = "Agregar Arista";
+        editEdgeWithoutDrag(data, callback);
+      },
+      editEdge: {
+        editWithoutDrag: function(data, callback) {
               document.getElementById('edge-operation').innerHTML = "Editar Arista";
               editEdgeWithoutDrag(data,callback);
             }
           }
+          
         }
       };
       network = new vis.Network(container, data, options);
+      
     }
 
     function editNode(data, cancelAction, callback) {
@@ -77,7 +82,9 @@ var nodes = null;
     }
 
     function saveNodeData(data, callback) {
-      data.label = document.getElementById('node-label').value;
+      data.id = document.getElementById('node-id').value;
+      data.label = document.getElementById('node-id').value;
+      vertices.push(data.id);
       clearNodePopUp();
       callback(data);
     }
@@ -103,10 +110,14 @@ var nodes = null;
 
     function saveEdgeData(data, callback) {
       if (typeof data.to === 'object')
-        data.to = data.to.id
+        data.to = data.to.id;
+       
       if (typeof data.from === 'object')
-        data.from = data.from.id
-      data.label = document.getElementById('edge-label').setAttribute("placeholder","uwu");
+        data.from = data.from.id;
+  
+      data.label = document.getElementById('edge-label').value;
+      aristas_from.push(data.from);
+      aristas_to.push(data.to);
       clearEdgePopUp();
       callback(data);
     }
@@ -115,3 +126,13 @@ var nodes = null;
       setDefaultLocale();
       draw();
     }
+    function show(){
+      console.log(vertices);
+      console.log(aristas_from);
+      console.log(aristas_to);
+    }
+
+
+    
+
+
